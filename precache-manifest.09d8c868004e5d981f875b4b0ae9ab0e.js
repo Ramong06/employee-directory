707dd57d751e63e7044e8a7b2ee16b3865bc5e31
RG-Mac <ramong5406@gmail.com>
2020-09-25 09:19:59 -0500

    Updates

---
+++ precache-manifest.09d8c868004e5d981f875b4b0ae9ab0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb87b5b80d04126e2cccf7fd07ae5a1a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "750de27ba7f2948c91e2",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a99a5eea9dfa815a85fc",
+    "url": "/my-app/static/js/2.8e1b47c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.8e1b47c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750de27ba7f2948c91e2",
+    "url": "/my-app/static/js/main.ba049d10.chunk.js"
+  },
+  {
+    "revision": "b3505c3d477b4db463a1",
+    "url": "/my-app/static/js/runtime-main.958628b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
